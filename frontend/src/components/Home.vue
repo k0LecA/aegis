9125d18ab1468a12f8aacb1acf565f22d2668348
k0LecA <jonasbutrimas@gmail.com>
2026-03-28 03:20:04 +0200

    front initial

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const streamUrl = "http://127.0.0.1:8000/video_feed";
+</script>
+
+<template>
+  <section id="center">
+    <img :src="streamUrl" alt="AEGIS Live Feed" class="camera-stream" />
+  </section>
+</template>
+
+<style scoped>
+  .camera-stream {
+    border: 2px solid #555;
+    border-radius: 4px;
+    width: 100%;
+    max-width: 800px;
+  }
+</style>
